e3ebbd7a7222f095597b67fc550216e8b2bb9da3
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-03 23:16:59 -0400

    Add files via upload

---
+++ one_million_numbers.py
@@ -0,0 +1,14 @@
+
+one_million = list(range(1, 1_000_000))
+for one_million_numbers in one_million:
+    print(one_million_numbers)
+
+print('The biggest number: ', max(one_million))
+print('The smallest number: ', min(one_million))
+print('Sum of all the numbers: ', sum(one_million))
+
+
+
+
+
+
