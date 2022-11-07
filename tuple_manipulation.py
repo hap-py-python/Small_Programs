887e6f965f09ecf8296da2c5af3de6b6192780ed
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-06 23:02:22 -0500

    Add files via upload

---
+++ tuple_manipulation.py
@@ -0,0 +1,13 @@
+print("Menu: ")
+menu = ('rice', 'noodles', 'salad', 'pilaf', 'water')
+
+for menu_items in menu:
+    print(menu_items.title())
+
+print("\n")
+# changes in menu
+print("New Menu: ")
+menu = ('milk', 'rice', 'water', 'orange juice', 'noodles')
+
+for new_menu_items in menu:
+    print(new_menu_items.title())
