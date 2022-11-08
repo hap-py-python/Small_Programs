1ee61638a75a01ee64cc229b2cb252c54660dcf4
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-07 20:41:14 -0500

    Add files via upload

---
+++ for_loop_and_if_statement.py
@@ -0,0 +1,11 @@
+# if statement + for loop
+
+pets = [ 'dogs', 'cats', 'shark', 'lion', 'parrots']
+
+for my_pets in pets:
+    if my_pets == 'shark':
+        print(f'{my_pets} is not  a pet')
+    elif my_pets == 'lion':
+        print(f'{my_pets} is not  a pet')
+    else:
+        print(my_pets)
