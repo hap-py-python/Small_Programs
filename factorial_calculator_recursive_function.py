5fb112218a5e147d94b7c6052e7adac824a8a649
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-11 00:18:16 -0500

    Add files via upload

---
+++ factorial_calculator_recursive_function.py
@@ -0,0 +1,15 @@
+# recursive function (function calls itself
+# factorial calculator
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+number = factorial(int(input("Your number: ")))
+
+print(number)
+
+
