bc7a5b9dad9aa756456b5c872ce57e9f46949010
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-15 14:38:44 -0500

    Add files via upload

---
+++ special_greeting.py
@@ -0,0 +1,13 @@
+# special greeting
+
+names = ['admin', 'olga', 'mary']
+
+for name in names:
+    if name == "admin":
+        print(f"Hello, {name.title()}, would you like to see the report </> ")
+    else:
+        print(f"Welcome, {name.title()}")
+
+
+
+
